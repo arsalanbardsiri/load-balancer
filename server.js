3621ac77aea3a4e49dd29216e03544f10585052e
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2025-12-10 10:13:30 -0800

    Feat: Add server_id to response for instance observability

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const os = require('os');
 const db = require('./db');
 const cors = require('cors');
 const app = express();
@@ -16,7 +17,7 @@ app.get('/users/:id', async (req, res) => {
             return res.status(404).json({ error: "User not found" });
         }
 
-        res.json(result.rows[0]);
+        res.json({ ...result.rows[0], server_id: os.hostname() });
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: "Internal Server Error" });
